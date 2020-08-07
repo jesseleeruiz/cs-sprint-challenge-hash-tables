7dc5af6e5c4d79345852ae629233fc1ed2c4a657
jesseleeruiz <jesseleeruiz@gmail.com>
2020-08-07 07:00:31 -1000

    Completed ex4.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,18 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    dataset = {}
+
+    for number in a:
+        change_neg_to_pos = abs(number)
+        if change_neg_to_pos in dataset:
+            dataset[change_neg_to_pos] += 1
+        else:
+            dataset[change_neg_to_pos] = 1
+            
+    sorted_dataset = sorted(dataset.items(), key = lambda pair: pair[1], reverse = True)
+    for pair in sorted_dataset:
+        if pair[1] > 1:
+            result.append(pair[0])
 
     return result
 
