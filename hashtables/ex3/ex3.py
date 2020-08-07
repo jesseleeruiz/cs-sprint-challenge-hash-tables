7a72ef575be7988e9893b42eecb0ca76000d55ac
jesseleeruiz <jesseleeruiz@gmail.com>
2020-08-07 06:58:58 -1000

    Completed ex3.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,18 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    dataset = {}
+    result = []
+
+    for lists in arrays:
+        for number in lists:
+            if number in dataset:
+                dataset[number] += 1
+            else:
+                dataset[number] = 1
+
+    sorted_dataset = sorted(dataset.items(), key = lambda pair: pair[1], reverse = True)
+    for pair in sorted_dataset:
+        if pair[1] > 1:
+            result.append(pair[0])
 
     return result
 
